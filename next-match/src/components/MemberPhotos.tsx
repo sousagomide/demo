87cb619337181aea32bab5691e29fc489fe18140
sousagomide <sousagomide@gmail.com>
2024-10-18 20:16:19 -0300

    upload

--- next-match/src/components/MemberPhotos.tsx
+++ next-match/src/components/MemberPhotos.tsx
@@ -6,7 +6,7 @@ import StarButton from './StarButton'
 import DeleteButton from './DeleteButton'
 import { Photo } from '@prisma/client'
 import { useRouter } from 'next/navigation'
-import { setMainImage } from '@/app/actions/userActions'
+import { deleteImage, setMainImage } from '@/app/actions/userActions'
 
 type Props = {
     photos: Photo[] | null
@@ -30,6 +30,14 @@ export default function MemberPhotos({photos, editing, mainImageUrl}: Props) {
         setLoading({isLoading: false, id: '', type: ''})
     }
 
+    const onDelete = async (photo: Photo) => {
+        if (photo.url === mainImageUrl) return null
+        setLoading({isLoading: true, id: photo.id, type: 'delete'})
+        await deleteImage(photo)
+        router.refresh()
+        setLoading({isLoading: false, id: '', type: ''})
+    }
+
     return (
         <div className='grid grid-cols-5 gap-3 p-5'>
             {photos && photos.map(photo => (
@@ -45,8 +53,10 @@ export default function MemberPhotos({photos, editing, mainImageUrl}: Props) {
                                     }
                                 />
                             </div>
-                            <div className='absolute top-3 right-3 z-50'>
-                                <DeleteButton loading={false}/>
+                            <div onClick={() => onDelete(photo)} className='absolute top-3 right-3 z-50'>
+                                <DeleteButton loading={
+                                    loading.isLoading && loading.type === 'delete' && loading.id === photo.id
+                                }/>
                             </div>    
                         </>
                     )}
